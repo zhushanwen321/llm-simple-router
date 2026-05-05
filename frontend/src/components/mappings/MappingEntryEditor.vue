0d5a6a21890207cc1541c33e79789c355e326985
ZZzzswszzZZ <zhushanwen321@hotmail.com>
2026-05-05 16:16:57 +0800

    fix: remove unused computed import in MappingEntryEditor

--- frontend/src/components/mappings/MappingEntryEditor.vue
+++ frontend/src/components/mappings/MappingEntryEditor.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { Plus, Trash2 } from 'lucide-vue-next'
 import { Button } from '@/components/ui/button'
